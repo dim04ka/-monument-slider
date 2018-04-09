dc682e6caa21595f095b6f1e88026d708f131a99
Сухоцкий Дмитрий Владимирович <Suhotski@MGS.BY>
2018-04-09 10:13:25 +0300

    add monum slider

---
+++ js/slider.js
@@ -0,0 +1,20 @@
+    	 $(document).ready(function() {
+			$("#content-slider").lightSlider({
+                loop:true,
+                keyPress:true
+            });
+        });    	 
+        
+        $(document).ready(function() {
+			$("#content-slider2").lightSlider({
+                loop:true,
+                keyPress:true
+            });
+		});
+        
+        $(document).ready(function() {
+			$("#content-slider3").lightSlider({
+                loop:true,
+                keyPress:true
+            });
+		});
